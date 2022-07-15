1f9bef7b03629c0537309450a397b194653ac51c
AlexITHoot <alex.pylypchuk@ithoot.com>
2022-07-15 12:15:57 +0300

    Product page

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,18 +1,54 @@
 import noUiSlider from 'nouislider';
 import SlimSelect from 'slim-select'
+import wNumb from 'wnumb';
 
 new SlimSelect({
   select: '#slim-select',
   showSearch: false,
 })
 
-var slider = document.getElementById('slider');
+var slider = document.querySelector('.range-slider');
 
-noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
+// noUiSlider.create(slider,{
+//         start: [200, 80000],
+//         connect: true,
+//         behaviour: 'tap',
+//         tooltips: [true, true],
+//         range: {
+//             'min': 0,
+//             'max': 100000
+//         }   
+// })
+  // slider.forEach((item)=>{
+   
+  // });
+  // let Option = function(){
+  //   this.start = start,
+  //   this.range = range,
+  // }
+  // class MyOption {
+  //   constructor(start,range){
+  //     this.start = start,
+  //     this.range = range
+  //   }
+  // }
+
+  // let y = new MyOption([0,100],{'min': 0,'max': 100000});
+  // let y2 
+
+    let c = {
+        start: [100, 800],
+        connect: true,
+        behaviour: 'tap',
+        tooltips: [wNumb({decimals: 0}), wNumb({decimals: 0})],
+        range: {
+            'min': 0,
+            'max': 1000
+        }
     }
-});
+    // noUiSlider.create(slider, c);   
+    let x = noUiSlider.create(slider,c);
+    console.log(x);
+// item[0]={
+//   start: [200, 800],
+// }
